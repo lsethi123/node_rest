b2a959bd145cec934c155ec19338cbdcb87880c5
lsethi123 <lalitsethi2003@gmail.com>
2014-08-28 16:37:15 +0530

    added projetcsroutes/project.js

--- routes/employee.js
+++ routes/employee.js
@@ -4,7 +4,7 @@ var MongoClient = require('mongodb').MongoClient,
 
 var mongoClient = new MongoClient(new Server('localhost', 27017));
 mongoClient.open(function(err, mongoClient) {
-    db = mongoClient.db("employeedb07");
+    db = mongoClient.db("employeedb05");
     db.collection('employees', {strict:true}, function(err, collection) {
         if (err) {
             console.log("The 'employees' collection doesn't exist. Creating it with sample data...");
@@ -15,6 +15,7 @@ mongoClient.open(function(err, mongoClient) {
 
 //list all employess
 exports.findAll = function(req, res) {
+    
     db.collection('employees', function(error, employee_collection) {
         employee_collection.find().toArray(function(error, items) {
             res.jsonp(items);
@@ -25,6 +26,7 @@ exports.findAll = function(req, res) {
 //get particular employee
 exports.findEmployee = function(req, res) {
     console.log('req.params',req.params);
+    console.log("new url");
     var id = req.params.id;
     db.collection('employees',function(error, employee_collection) {
       if( error ) res.jsonp(error)
@@ -40,7 +42,6 @@ exports.findEmployee = function(req, res) {
 //create new employee
 exports.createEmployee = function(req,res){
     console.log(req.body);
-
     db.collection('employees', function(error, employee_collection) {
       if( error ) res.jsonp(error)
       else {
@@ -53,14 +54,14 @@ exports.createEmployee = function(req,res){
 };
 // update the particular employee
 exports.updateEmployee = function  (req,res) {
-    var update_emp_oject= req.body;
-    var emp_id = update_emp_oject['_id'];
-    delete(update_emp_oject['_id']);
-    //console.log('update_emp_oject',update_emp_oject)
+    var update_emp_object= req.body;
+    var emp_id = update_emp_object['_id'];
+    delete(update_emp_object['_id']);
+    //console.log('update_emp_object',update_emp_object)
     db.collection('employees',function(error,employee_collection){
         if(error) res.jsonp(error)
         else{
-            employee_collection.update({_id: employee_collection.db.bson_serializer.ObjectID.createFromHexString(emp_id)},update_emp_oject,
+            employee_collection.update({_id: employee_collection.db.bson_serializer.ObjectID.createFromHexString(emp_id)},update_emp_object,
              function(){
                 res.jsonp({status:"OK",message:"Employee updated Sucessfully...."});
             });
@@ -81,18 +82,25 @@ exports.deleteEmployee = function(req,res){
         }
     });
 };
-// exports.findByManager = function(req, res) {
-//     var id = parseInt(req.params.id);
-//     console.log('findByManager: ' + id);
-//     db.collection('employees', function(err, collection) {
-//         collection.find({'managerId': id}).toArray(function(err, items) {
-//             console.log(items);
-//             res.jsonp(items);
-//         });
-//     });
-// };
+exports.getAllManager = function(req,res){
+    db.collection('employees', function(error, employee_collection) {
+        employee_collection.find({ actAsManager : true },{ _id: 1, name: 1 }).toArray(function(error, items) {
+           
+            res.jsonp(items);
+        });
+    });
+};
 
-
+exports.getEmployeeByManager = function(req,res){
+    var managerId =req.params.id;
+    //console.log("managerId",managerId);
+    db.collection('employees', function(error, employee_collection) {
+        employee_collection.find({ "manager._id" : managerId },{ _id: 1, name: 1,designation: 1 }).toArray(function(error, items) {
+            //console.log('items',items);
+            res.jsonp(items);
+        });
+    });
+};
  
 /*--------------------------------------------------------------------------------------------------------------------*/
 // Populate database with sample data -- Only used once: the first time the application is started.
@@ -100,21 +108,25 @@ exports.deleteEmployee = function(req,res){
 var populateDB = function() {
  
     console.log("Populating employee database...");
-    var employees = [
-        {"id": 1, "name": "James King", "managerId": 0, managerName: "", "designation": "President and CEO", "phone": "617-000-0001",  "email": "jking@fakemail.com", "address": "Boston, MA"},
-        {"id": 2, "name": "Julie Taylor", "managerId": 1, managerName: "James King", "designation": "VP of Marketing",  "phone": "617-000-0002", "email": "jtaylor@fakemail.com", "address": "Boston, MA"},
-        {"id": 3, "name": "Eugene Lee", "managerId": 1, managerName: "James King", "designation": "CFO",  "phone": "617-000-0003",  "email": "elee@fakemail.com", "address": "Boston, MA"},
-        {"id": 4, "name": "John Williams", "managerId": 1, managerName: "James King", "designation": "VP of Engineering", "phone": "617-000-0004", "email": "jwilliams@fakemail.com", "address": "Boston, MA"},
-        {"id": 5, "name": "Ray Moore", "managerId": 1, managerName: "James King", "designation": "VP of Sales",  "phone": "617-000-0005", "email": "rmoore@fakemail.com", "address": "Boston, MA"},
-        {"id": 6, "name": "Paul Jones", "managerId": 4, managerName: "John Williams", "designation": "QA Manager",  "phone": "617-000-0006", "email": "pjones@fakemail.com", "address": "Boston, MA"},
-        {"id": 7, "name": "Paula Gates", "managerId": 4, managerName: "John Williams", "designation": "Software Architect",  "phone": "617-000-0007", "email": "pgates@fakemail.com", "address": "Boston, MA"},
-        {"id": 8, "name": "Lisa Wong", "managerId": 2, managerName: "Julie Taylor", "designation": "Marketing Manager", "phone": "617-000-0008",  "email": "lwong@fakemail.com", "address": "Boston, MA"},
-        {"id": 9, "name": "Gary Donovan", "managerId": 2, managerName: "Julie Taylor", "designation": "Marketing Manager",  "phone": "617-000-0009", "email": "gdonovan@fakemail.com", "address": "Boston, MA"},
-        {"id": 10, "name": "Kathleen Byrne", "managerId": 5, managerName: "Ray Moore", "designation": "Sales Representative", "phone": "617-000-0010",  "email": "kbyrne@fakemail.com", "address": "Boston, MA"},
-        {"id": 11, "name": "Amy Jones", "managerId": 5, managerName: "Ray Moore", "designation": "Sales Representative", "phone": "617-000-0011", "email": "ajones@fakemail.com", "address": "Boston, MA"},
-        {"id": 12, "name": "Steven Wells", "managerId": 4, managerName: "John Williams", "designation": "Software Architect", "phone": "617-000-0012",  "email": "swells@fakemail.com", "address": "Boston, MA"}
-    ];
+    // var employees = [
+    //     {"id": 1, "name": "James King", "managerId": 0, managerName: "", "designation": "President and CEO", "phone": "617-000-0001",  "email": "jking@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 2, "name": "Julie Taylor", "managerId": 1, managerName: "James King", "designation": "VP of Marketing",  "phone": "617-000-0002", "email": "jtaylor@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 3, "name": "Eugene Lee", "managerId": 1, managerName: "James King", "designation": "CFO",  "phone": "617-000-0003",  "email": "elee@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 4, "name": "John Williams", "managerId": 1, managerName: "James King", "designation": "VP of Engineering", "phone": "617-000-0004", "email": "jwilliams@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 5, "name": "Ray Moore", "managerId": 1, managerName: "James King", "designation": "VP of Sales",  "phone": "617-000-0005", "email": "rmoore@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 6, "name": "Paul Jones", "managerId": 4, managerName: "John Williams", "designation": "QA Manager",  "phone": "617-000-0006", "email": "pjones@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 7, "name": "Paula Gates", "managerId": 4, managerName: "John Williams", "designation": "Software Architect",  "phone": "617-000-0007", "email": "pgates@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 8, "name": "Lisa Wong", "managerId": 2, managerName: "Julie Taylor", "designation": "Marketing Manager", "phone": "617-000-0008",  "email": "lwong@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 9, "name": "Gary Donovan", "managerId": 2, managerName: "Julie Taylor", "designation": "Marketing Manager",  "phone": "617-000-0009", "email": "gdonovan@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 10, "name": "Kathleen Byrne", "managerId": 5, managerName: "Ray Moore", "designation": "Sales Representative", "phone": "617-000-0010",  "email": "kbyrne@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 11, "name": "Amy Jones", "managerId": 5, managerName: "Ray Moore", "designation": "Sales Representative", "phone": "617-000-0011", "email": "ajones@fakemail.com", "address": "Boston, MA"},
+    //     {"id": 12, "name": "Steven Wells", "managerId": 4, managerName: "John Williams", "designation": "Software Architect", "phone": "617-000-0012",  "email": "swells@fakemail.com", "address": "Boston, MA"}
+    // ];
+    var employees =[
 
+        {"name": "James King", "manager":{}, "designation": "President and CEO", "phone": "617-000-0001",  "email": "jking@fakemail.com", "address": "Boston, MA",actAsManager:true}
+    
+    ];
  
     db.collection('employees', function(err, collection) {
         collection.insert(employees, {safe:true}, function(err, result) {});
